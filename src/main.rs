147df20c36a942328051a9420a20637c07001668
XENOr-god <xenor.god@gmail.com>
2026-04-09 21:23:34 +0700

    feat: run propagation demo from core entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 mod propagation;
 
+use propagation::Graph;
+
 fn main() {
-    println!("XENØr core propagation engine initialized");
+    let mut graph = Graph::new();
+
+    graph.add_edge("Protocol", "NodeA");
+    graph.add_edge("NodeA", "NodeB");
+    graph.add_edge("NodeA", "NodeC");
+
+    let result = graph.propagate("NodeA");
+
+    println!("XENØr core engine starting");
+    println!("Propagation from NodeA -> {:?}", result);
 }
